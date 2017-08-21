1483354e426a28a1c6601048426be9208e457182
akaorignal <torir.adams@gmail.com>
2017-08-20 22:39:33 -0700

    Add files via upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,23 @@
 //questions
-//testing 
-/*var timer = 9;
-console.log(timer)
+//testing  timer = 9;
 
 window.onload = function () {
 //countdown of the game
-setTimeout(countdown, 1000 * 5);
+setTimeout(countdown, 1000 * 20);
+setTimeout(countdown2, 1000 * 10);
+
 
 function countdown(){
-  $("#timer").append("countdown");
-  console.log("10 seconds left");
-}*/
+  $("#countdown").append("Time is running out");
+  console.log("10 seconds left");}
+  function countdown2(){
+  $("#countdown2").append("Almost over..");
+  console.log("10 seconds left");}
+
+
+
+}
+
 
 //questions
 //Is Nicki Minaj Better Than Remy Ma?
@@ -53,35 +60,11 @@ function submitAnswers(){
 	 alert( "you got" + " " + score + " " + "out of" + " " + total + " " + "correct.");
 
 	 
-	 window.onload = function () { }
 
 // tells the user about the amount of time left 
-/*
-setTimeout(fiveSeconds, 1000 * 5);
-setTimeout(tenSeconds, 1000 * 10);
-setTimeout(timeUp, 1000 * 15);
-//  Step 3:
-//  Fill in the blanks to these functions.
-function fiveSeconds() {
-  // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
-  // console log 10 seconds left
- $("#countdown").append("dddd");
-  console.log("10 seconds left");
-}
-function tenSeconds() {
-  // in the element with an id of time-left add an h2 saying About 5 Seconds Left!
-  // console log 5 seconds left
- $("#countdown").append("<h3>About 5 Seconds Left!</h3>");
-  console.log("5 seconds left");
-}
-function timeUp() {
-  // in the element with an id of time-left add an h2 saying Time's Up!
-  // console log done
-  console.log("done");
- // $("#countdown").append("<h2>Time's Up!</h2>");
-  console.log("time is up");
 
-} */
+
+
 
 function results (){
 
@@ -90,6 +73,7 @@ function results (){
 	else
 		alert("gjj")
 }
+
 
 }//end
 
